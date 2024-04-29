21f2103f6e336d7d852dccd58205f74d17aeae7f
Waldir Júnior <waldirjunior.gpn@gmail.com>
2024-04-29 19:06:01 -0300

    primeiro commit

---
+++ src/test/java/br/com/erp/apierp/ApiErpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erp.apierp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
